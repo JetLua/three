615a501b115cb00557b1cf8f2dc3bca19b2c14e9
鲁飞 <fei.lu0@geely.com>
2021-01-14 18:02:03 +0800

    :clap: 📦

--- src/scene/entry/index.ts
+++ src/scene/entry/index.ts
@@ -10,6 +10,7 @@ let group: THREE.Group
 function init() {
   group = new THREE.Group()
   addMap()
+  addBox()
   camera.position.z = 10
   stage.add(group)
 }
@@ -27,11 +28,14 @@ async function addMap() {
   const d3 = await import('d3-geo')
   const data = await import('@/static/map.china.json')
   const project = d3.geoMercator().center([104, 37.5]).scale(10).translate([0, 0])
-  const texture = window.textureCache['1.jpg']
 
   project.reflectY(true)
 
   for (const feature of data.features) {
+    if (!feature.properties.center) continue
+
+    const center = project(feature.properties.center as any)
+
     for (const coord of feature.geometry.coordinates) {
       for (const points of coord) {
         const shape = new THREE.Shape()
@@ -41,17 +45,40 @@ async function addMap() {
           i ? shape.lineTo(x, y) : shape.moveTo(x, y)
         })
 
-        const line = new THREE.Line(
-          new THREE.ShapeGeometry(shape),
+        const texture = await loader.loadAsync('1.jpg') as THREE.Texture
+        // const texture = window.textureCache['1.jpg'].clone()
+        texture.wrapS = THREE.RepeatWrapping
+        texture.wrapT = THREE.RepeatWrapping
+        // texture.offset.x = center[0]
+        // texture.offset.y = center[1]
+
+        const line = new THREE.Mesh(
+          // new THREE.ShapeGeometry(shape),
+          new THREE.ExtrudeGeometry(shape, {depth: .1, bevelEnabled: false}),
           new THREE.MeshBasicMaterial({
-            color: 0x00bcd4
+            map: texture
           })
         )
 
-        line.computeLineDistances()
+        console.log(line.geometry.faceVertexUvs)
 
         group.add(line)
       }
     }
   }
+}
+
+async function addBox() {
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(.1, .1, .1),
+    new THREE.MeshBasicMaterial({
+      map: window.textureCache['1.jpg']
+    })
+  )
+
+  console.log(box.geometry.faceVertexUvs)
+
+
+
+  group.add(box)
 }
